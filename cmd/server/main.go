6793a9356eb59738bdb7181c1d88e573282bfe46
Елена Григорьева <gev_6484_gev@icloud.com>
2025-11-17 16:21:47 +0300

    add handler

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/lenchik-en/lbs_server/internal/app"
+	"github.com/lenchik-en/lbs_server/internal/db"
+)
+
+func main() {
+	godotenv.Load("../../.env")
+
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		log.Fatalf("no path in DB_DSN")
+	}
+
+	locatedb := db.NewLocateDB(dsn)
+	err := locatedb.Connect()
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer locatedb.DB.Close()
+
+	app.Run(locatedb)
+}
